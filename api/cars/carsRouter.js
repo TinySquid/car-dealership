ece67e51b9b3331e66e2e4074e3b03a73d3bac9d
Michael Nunes <miketnunes@gmail.com>
2019-12-10 12:54:02 -0800

    Add GET all cars route

--- api/cars/carsRouter.js
+++ api/cars/carsRouter.js
@@ -1,5 +1,21 @@
 const router = require('express').Router();
 
+const carsDB = require('./carsDB');
 
+//* GET / - Returns all cars in inventory.
+router.get("/", (req, res) => {
+  carsDB.get()
+    .then(cars => {
+      if (cars.length > 0) {
+        res.status(200).json(cars);
+      } else {
+        res.status(404).json({ message: "No cars found" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Error getting cars" });
+    });
+});
 
 module.exports = router;
