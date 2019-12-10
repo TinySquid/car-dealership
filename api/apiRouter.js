d6f5269cea4c4515fdd415c79724ea895f4046bd
Michael Nunes <miketnunes@gmail.com>
2019-12-10 13:53:15 -0800

    Add sales routes to api router

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const carsRouter = require('./cars/carsRouter');
+const salesRouter = require('./sales/salesRouter');
 
 router.use("/cars", carsRouter);
+router.use("/sales", salesRouter);
 
 module.exports = router;
