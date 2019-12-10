25dbfcd0d025715558c3a5898c37d735930c0f38
Michael Nunes <miketnunes@gmail.com>
2019-12-10 11:41:11 -0800

    Add cars table migration file

--- data/migrations/20191210105225_cars.js
+++ data/migrations/20191210105225_cars.js
@@ -1,8 +1,22 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('cars', tbl => {
+    //PRIMARY KEY
+    tbl.increments('carId');
+
+    //REQUIRED 
+    tbl.string('vin', 17).unique().notNullable();
+    tbl.string('make').notNullable();
+    tbl.string('model').notNullable();
+    tbl.integer('year').notNullable();
+    tbl.integer('mileage').notNullable();
+
+    //UNKNOWN AT TIME OF INSERT
+    tbl.string('transmission').nullable();
+    tbl.string('title').nullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('cars');
 };
