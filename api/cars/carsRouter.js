7ad5cd33faf3320e68fb901a408709f4c77ef782
Michael Nunes <miketnunes@gmail.com>
2019-12-10 13:05:32 -0800

    Add POST new car and middleware

--- api/cars/carsRouter.js
+++ api/cars/carsRouter.js
@@ -35,4 +35,56 @@ router.get("/:id", (req, res) => {
     });
 });
 
+//* POST / - Adds a new car.
+router.post("/", validateCar, (req, res) => {
+  const car = req.body;
+
+  carsDB.insert(car)
+    .then(ids => {
+      carsDB.get(ids[0])
+        .then(car => {
+          //Send new car back to client
+          res.status(201).json(car);
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({
+            message: "Error getting car"
+          });
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      if (error.errno === 19) {
+        res.status(400).json({ message: "Car with specific VIN already exists in inventory!" });
+      } else {
+        res.status(500).json({ message: "Error adding a car" });
+      }
+    });
+});
+
+//MIDDLEWARE
+function validateCar(req, res, next) {
+  const { vin, make, model, year, mileage } = req.body;
+
+  if (vin && make && model && year && mileage) {
+    next();
+  } else {
+    res.status(400).json({ message: "Please provide a VIN, make, model, year, and mileage of car" });
+  }
+}
+
+function validateCarId(req, res, next) {
+  const id = req.params.id;
+
+  carsDB.get(id)
+    .then(car => {
+      if (car) {
+        next();
+      } else {
+        res.status(400).json({ message: "Invalid car ID" });
+      }
+    })
+}
+
 module.exports = router;
